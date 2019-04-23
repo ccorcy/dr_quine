0c2fadbbea4e5d05f6b938d46ce677b6387575c1
Charles CORCY <ccorcy@student.42.fr>
2019-04-23 14:35:47 +0200

    fix(colleen): clang flag support

--- Colleen.c
+++ Colleen.c
@@ -1,1 +1,2 @@
-void quine(){char* data="void quine(){char* data=%c%s%c;printf(data, 34, data, 34, 47, 47, 47, 47, 10);}%c* comment outside *%cint main(){%c* comment in main *%cquine();}%c";printf(data, 34, data, 34, 47, 47, 47, 47, 10);}/* comment outside */int main(){/* comment in main */quine();}
+#include <stdio.h>
+void quine(){char* data="#include <stdio.h>%cvoid quine(){char* data=%c%s%c;printf(data, 10, 34, data, 34, 47, 47, 47, 47, 10);}%c* comment outside *%cint main(){%c* comment in main *%cquine();}%c";printf(data, 10, 34, data, 34, 47, 47, 47, 47, 10);}/* comment outside */int main(){/* comment in main */quine();}
