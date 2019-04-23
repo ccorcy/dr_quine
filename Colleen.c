5c6afd7da853cb3ddcb8d0bb6345f2eaa09f78b0
Charles CORCY <ccorcy@student.42.fr>
2019-04-23 14:19:39 +0200

    feat(colleen): need comment in colleen but works for now

--- Colleen.c
+++ Colleen.c
@@ -1,1 +1,1 @@
-void quine(){char* data="void quine(){char *data=%c%s%c;printf(data, 34, data, 34);}int main(){quine();}";printf(data, 34, data, 34);}int main(){quine();}
+void quine(){char* data="void quine(){char* data=%c%s%c;printf(data, 34, data, 34, 10);}int main(){quine();}%c";printf(data, 34, data, 34, 10);}int main(){quine();}
