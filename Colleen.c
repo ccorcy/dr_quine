ae8b966b60c08709950ed4b94370ff148624a873
Charles CORCY <ccorcy@student.42.fr>
2019-04-23 14:29:42 +0200

    feat(colleen): colleen done

--- Colleen.c
+++ Colleen.c
@@ -1,1 +1,1 @@
-void quine(){char* data="void quine(){char* data=%c%s%c;printf(data, 34, data, 34, 10);}int main(){quine();}%c";printf(data, 34, data, 34, 10);}int main(){quine();}
+void quine(){char* data="void quine(){char* data=%c%s%c;printf(data, 34, data, 34, 47, 47, 47, 47, 10);}%c* comment outside *%cint main(){%c* comment in main *%cquine();}%c";printf(data, 34, data, 34, 47, 47, 47, 47, 10);}/* comment outside */int main(){/* comment in main */quine();}
