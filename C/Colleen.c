a784b0e608c2ed22d6ddee0307414bfcd71c44df
Charles CORCY <ccorcy@student.42.fr>
2019-04-28 16:25:50 +0200

    feat(C): better comment for Colleen + Avoid relink

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
-void quine(){char* data="#include <stdio.h>%cvoid quine(){char* data=%c%s%c;printf(data, 10, 34, data, 34, 47, 47, 47, 47, 10);}%c* comment outside *%cint main(){%c* comment in main *%cquine();}%c";printf(data, 10, 34, data, 34, 47, 47, 47, 47, 10);}/* comment outside */int main(){/* comment in main */quine();}
+void quine(){char* data="#include <stdio.h>%cvoid quine(){char* data=%c%s%c;printf(data, 10, 34, data, 34, 10, 47, 10, 10, 47, 10, 10, 47, 10, 10, 47, 10, 10);}%c%c*%c    comment outside%c*%c%cint main(){%c%c*%c    comment in main%c*%c%cquine();}%c";printf(data, 10, 34, data, 34, 10, 47, 10, 10, 47, 10, 10, 47, 10, 10, 47, 10, 10);}
+/*
+    comment outside
+*/
+int main(){
+/*
+    comment in main
+*/
+quine();}
