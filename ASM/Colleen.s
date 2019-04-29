0905929f56449bd3e901f06a4a1fbf8fc20e3d4c
Charles CORCY <ccorcy@student.42.fr>
2019-04-29 18:04:56 +0200

    feat(asm): done colleen in asm

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,15 +1,29 @@
-extern _printf ; the C function, to be called
-section .data
-data: db "extern printf", 10, 0
-.len: equ $ - data
-global main
-    section .text
-
-; Ceci est un commentaire en dehors des fonctions du programme
-
-main:   push rbx
-        ; Ceci est un commentaire dans la fonction main
-        mov rdi, data
-        call _printf
-        pop rbx
-        ret
+section .data ; this is a comment
+data db "section .data %c this is a comment%cdata db %c%s%c%csection .text%cextern _printf%cglobal _start%cglobal _main%c_start:%c_main:%cpush rbp%cmov rbp, rsp%csub rsp, 24%cmov rcx, 0%cloop_break_line:%cpush 10%cinc rcx%ccmp rcx, 27%cje quine%cjmp loop_break_line%cquine:%clea rdi, [rel data]%cmov rsi, 59%cmov rdx, 10%cmov rcx, 34%clea r8, [rel data]%cmov r9, 34%ccall _printf%cmov rsp, rbp%cleave%cret"
+section .text
+extern _printf
+global _start
+global _main
+_start:
+_main:
+push rbp
+mov rbp, rsp
+sub rsp, 24
+mov rcx, 0
+loop_break_line:
+push 10
+inc rcx
+cmp rcx, 27
+je quine
+jmp loop_break_line
+quine:
+lea rdi, [rel data]
+mov rsi, 59
+mov rdx, 10
+mov rcx, 34
+lea r8, [rel data]
+mov r9, 34
+call _printf
+mov rsp, rbp
+leave
+ret
