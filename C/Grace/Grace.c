357c13df6e83e0074e9d1e62c0d9f17e6afe87e7
Charles CORCY <ccorcy@student.42.fr>
2019-04-30 13:53:38 +0200

    fix(Grace): update comment in Grace to match comment in subject

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-#define DATA "#include <stdio.h>%c#define DATA %c%s%c%c#define MAIN() GRACE(DATA)%c#define GRACE(x)int main(){%c*This program will print its own source when run. *%cFILE *fp;fp = fopen(%c./Grace_kid.c%c, %cw+%c);fprintf(fp, x, 10, 34, x, 34, 10, 10, 47, 47, 34, 34, 34, 34, 10, 10, 10);fclose(fp);return 0;}%c%cMAIN()%c"
+#define DATA "#include <stdio.h>%c#define DATA %c%s%c%c#define MAIN() GRACE(DATA)%c#define GRACE(x)int main(){FILE *fp;fp = fopen(%c./Grace_kid.c%c, %cw+%c);fprintf(fp, x, 10, 34, x, 34, 10, 10, 34, 34, 34, 34, 10, 47, 10, 10, 47, 10, 10);fclose(fp);return 0;}%c%c*%c    This program will print its own source when run.%c*%c%cMAIN()%c"
 #define MAIN() GRACE(DATA)
-#define GRACE(x)int main(){/*This program will print its own source when run. */FILE *fp;fp = fopen("./Grace_kid.c", "w+");fprintf(fp, x, 10, 34, x, 34, 10, 10, 47, 47, 34, 34, 34, 34, 10, 10, 10);fclose(fp);return 0;}
-
+#define GRACE(x)int main(){FILE *fp;fp = fopen("./Grace_kid.c", "w+");fprintf(fp, x, 10, 34, x, 34, 10, 10, 34, 34, 34, 34, 10, 47, 10, 10, 47, 10, 10);fclose(fp);return 0;}
+/*
+    This program will print its own source when run.
+*/
 MAIN()
