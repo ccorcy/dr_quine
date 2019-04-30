8554846ca81dd86dd5a02fe1811fb5066c01d879
Charles CORCY <ccorcy@student.42.fr>
2019-04-30 11:10:08 +0200

    feat(asm): add comment to Colleen in main

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -1,22 +1,23 @@
 section .data ; this is a comment
-data db "section .data %c this is a comment%cdata db %c%s%c%csection .text%cextern _printf%cglobal _start%cglobal _main%c_start:%c_main:%cpush rbp%cmov rbp, rsp%csub rsp, 24%cmov rcx, 0%cloop_break_line:%cpush 10%cinc rcx%ccmp rcx, 27%cje quine%cjmp loop_break_line%cquine:%clea rdi, [rel data]%cmov rsi, 59%cmov rdx, 10%cmov rcx, 34%clea r8, [rel data]%cmov r9, 34%ccall _printf%cmov rsp, rbp%cleave%cret"
+data db "section .data %c this is a comment%cdata db %c%s%c%csection .text%cextern _printf%cglobal _start%cglobal _main%c_start:%c_main:%cpush rbp %c comment in main%cmov rbp, rsp%csub rsp, 24%cmov rcx, 0%cloop_break_line:%cpush 10%cinc rcx%ccmp rcx, 29%cje quine%cjmp loop_break_line%cquine:%cadd word [rbp - 200], 49%clea rdi, [rel data]%cmov rsi, 59%cmov rdx, 10%cmov rcx, 34%clea r8, [rel data]%cmov r9, 34%ccall _printf%cmov rsp, rbp%cleave%cret"
 section .text
 extern _printf
 global _start
 global _main
 _start:
 _main:
-push rbp
+push rbp ; comment in main
 mov rbp, rsp
 sub rsp, 24
 mov rcx, 0
 loop_break_line:
 push 10
 inc rcx
-cmp rcx, 27
+cmp rcx, 29
 je quine
 jmp loop_break_line
 quine:
+add word [rbp - 200], 49
 lea rdi, [rel data]
 mov rsi, 59
 mov rdx, 10
