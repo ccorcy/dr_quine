b713c07bb8e2bc7e100969938e9b69708c89ba3a
Charles CORCY <ccorcy@student.42.fr>
2019-05-07 17:25:22 +0200

    feat(js): done colleen

---
+++ JS/Colleen/Colleen.js
@@ -0,0 +1,10 @@
+function quine() {
+    /*
+        Inside
+    */
+    console.log(''+quine+'')
+}
+/*
+    outside
+*/
+(function main() {quine();console.log('/*\n    outside\n*/');console.log('('+main+')()')})()
